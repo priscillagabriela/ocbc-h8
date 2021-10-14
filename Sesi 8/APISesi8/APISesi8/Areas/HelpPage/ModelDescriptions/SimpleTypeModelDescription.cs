8a5f15fdac64602cd4a7d9d8c71c5d2933ee7de7
unknown <email.com>
2021-10-14 19:19:46 +0700

    Sesi 8

---
+++ Sesi 8/APISesi8/APISesi8/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APISesi8.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Sesi 8/APISesi8/APISesi8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APISesi8.WebApiApplication" Language="C#" %>
